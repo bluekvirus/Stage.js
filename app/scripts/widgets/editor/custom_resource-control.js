a6614b916262f23fb4a21d88a0e8d459e7643f09
Tim.Liu <bluekvirus@gmail.com>
2013-06-20 19:20:14 +0800

    Rewriting resource-control (privilege) editor...
    data source: /logic/Resources/list

--- app/scripts/widgets/editor/custom_resource-control.js
+++ app/scripts/widgets/editor/custom_resource-control.js
@@ -1,75 +1,27 @@
 /**
  * 
- * This is the Resource Control Editor used for User privilege 
- * assignment. It is used in the Role entity and the same concept
- * can be used for any resource control mech. 
- *
- * Concept: (type here is not the mapping type, but a meta describing resource type)
- * register>> 
- *     Resource <- Signatures <- Mappings
- * assign>>
- *     Role <- (Resource.(type).Mappings) <- Signatures (yes/no)
- * resolve>>
- *     (req.path) -> (/:type/:Resource/...) -> User.Role.Privileges[Resource.(type).Mappings]
- *
- * For performance reason it is better to re-group the Mappings inside a Resource
- * to be grouped with types e.g /:type/:Resource when saved in a Role. Specifically,
- * 
- *     mapping.split('/') =>   [0]: should always be "";
- *                             [1]: type;
- *                             [2]: Resource name; [null] means "Index Page";
- *
- * Note:: We still use the full mappings(+mapping type) when try to match with req.path
- * in authorization middleware.
- *                             
+ * Base Editor Example. [Modified]
+ * Based on [https://github.com/powmedia/backbone-forms#custom-editors]
  *
  * @author Tim.Liu
- * @created 2013.04.18
+ * @update 2013.04.18
  * 
  */
 (function(){
 
-    var ResourceCollection = Backbone.Collection.extend({
-        model: Backbone.Model.extend({}),
-    });
-
-    var ResourceItem = Backbone.Marionette.ItemView.extend({
-        template: '#custom-tpl-widget-editor-resource-item',
-        className: 'custom-form-editor-resource-control-item',
-        tagName: 'tr',
-
-        events: {
-            'click li': 'toggleSelection',
-            'recover': 'recoverSelection',
-        },
-
-        toggleSelection: function(e){
-            var target = $(e.target);
-            target.toggleClass('btn-success selected');
-        },
-
-        recoverSelection: function(e){
-            var target = $(e.target);
-            target.toggleClass('btn-success selected active');
-        },
-
-    });
-
-    var ViewWrap = Backbone.Marionette.CompositeView.extend({
+    var ViewWrap = Backbone.Marionette.Layout.extend({
         template: '#custom-tpl-widget-editor-resource-control-wrap',
         className: 'custom-form-editor-resource-control custom-editor-wrap',
-        itemView: ResourceItem,
-        itemViewContainer: '.body tbody',
 
-        ui: {
+        regions: {
             header: '.header',
             body: '.body',
             footer: '.footer'
         },
 
-        events: {
-            'click .custom-form-editor-resource-control-item li': 'markSelection'
-        },
+        // events: {
+
+        // },
 
         initialize: function(options){
             this._options = options;
@@ -77,47 +29,8 @@
 
         onRender: function(){
             //TODO::
-            this.$el.find('.btn').button().popover({
-                placement: 'top',
-                trigger: 'hover',
-                html: 'true',
-                container: 'body',
-                title: '<strong>Mappings</strong>'
-            });
-        },
-
-        getValue: function(){
-            return this.privileges;
-        },
-
-        setValue: function(val){
-            this.privileges = val || {} ;
-            var that = this;
-            _.each(val, function(signatures, resouce){
-                //mark
-                _.each(signatures, function(bool, sig){
-                    that.$el.find('.btn[resource="'+resouce+'"][signature="'+sig+'"]').trigger('recover');
-                });
-            });
-        },
-
-        markSelection: function(e){
-            var target = $(e.target);
-            var r = target.attr('resource'), s = target.attr('signature');
-            if(target.hasClass('selected')){
-                this.privileges[r] = this.privileges[r] || {};
-                this.privileges[r][s] = true;
-            }else{
-                try{
-                    delete this.privileges[target.attr('resource')][target.attr('signature')];
-                }catch(e){
-                    console.log('omit deletion privilege...');
-                }
-                
-            }
         }
     });
-
 
     Backbone.Form.editors['ResourceControl'] = Backbone.Form.editors.Base.extend({
 
@@ -152,23 +65,21 @@
         },
 
         render: function() {
-            Application.DataCenter.resolve('Resource', this.form, _.bind(function(data){
-                this.delegatedEditor = new ViewWrap({form:this.form, editor:this, collection: new ResourceCollection(data)});
-                this.delegatedEditor.listenTo(this.form, 'close', this.delegatedEditor.close);
-                this.$el.html(this.delegatedEditor.render().el); 
-                this.setValue(this.value);               
-            }, this));
+            //this.setValue(this.value);
 
+            this.delegatedEditor = new ViewWrap({form:this.form, editor:this});
+            this.delegatedEditor.listenTo(this.form, 'close', this.delegatedEditor.close);
+            this.$el.html(this.delegatedEditor.render().el);
         
             return this;
         },
 
         getValue: function() {
-            return this.delegatedEditor.getValue();
+            //return this.$el.val();
         },
 
         setValue: function(value) {
-            this.delegatedEditor.setValue(value);
+            //this.$el.val(value);
         },
 
         focus: function() {
@@ -190,34 +101,9 @@
     });
 })();
 
-/**
- * =====================
- * Templates and Helpers
- * =====================
- */
-Template.extend('custom-tpl-widget-editor-resource-item', [
-
-    '<td><span class=""><i class="icon-cog"></i> {{name}}</span></td>',
-    '<td><ul class="btn-group" data-toggle="buttons-checkbox">',
-        ' {{#each signatures}}',
-            '<li class="btn" resource="{{../name}}" signature="{{this.name}}" data-content="{{#if mappings}}{{#each mappings}}{{showSignatureMapping ../type this}}{{/each}}{{else}}<p class=\'label\'>N/A</p>{{/if}}">{{this.name}}</li>',
-        '{{/each}}',
-    '</ul></td>'
-
-]);
 
 Template.extend('custom-tpl-widget-editor-resource-control-wrap',[
     '<div class="header"></div>',
-    '<div class="body"><table class="table table-striped"><thead><tr><th>Resources</th><th>Signatures (e.g <span class="label label-warning">loosely</span> <span class="label label-important">strictly</span>)</th></tr></thead><tbody></tbody></table></div>',
+    '<div class="body"></div>',
     '<div class="footer"></div>'
 ]);
-
-Handlebars.registerHelper('showSignatureMapping', function(type, mapping){
-    try{
-        var parts = mapping.split(':');
-        return '<p><span class="label '+(type==='strictly'?'label-important':'label-warning')+'">'+parts[0]+'</span> '+parts[1]+'</p>';
-    }catch(e){
-        console.log(e);
-    }
-
-});
