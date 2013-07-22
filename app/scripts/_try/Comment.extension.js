9e4c30f1bf8da8689d9b3c1b63022384decafa31
Tim Liu <bluekvirus@gmail.com>
2013-07-22 22:13:09 +0800

    Refined client grid filter in comment.module.extesion.js
    ToDo:
    1. make filter per column input respond;
    2. multi-column progressive filter;
    3. make sub-doc grid locally sortable; (optional)

--- app/scripts/_try/Comment.extension.js
+++ app/scripts/_try/Comment.extension.js
@@ -54,10 +54,15 @@
         },
         'View.DataGrid.events': { //Todo::
 
+            'click i.icon-filter' : 'filtColumn',
+            'focusout input.columFilter' : 'closeFilt',
         },
         'View.DataGrid': { //Todo::
             //mode: undefined/editor
             onRenderPlus: function(view, mode) { //Todo::
+                if(mode && view.options.mode === 'subDoc'){
+                    view.$el.find('th').off();
+                }
                 //a. apply the client side search filter
                 var fields = _.pluck(view.columns, 'name');
                 fields.pop();fields.shift();//get the _select_, _action_ columns out;
@@ -72,7 +77,30 @@
                     //render it to the datagrid header ct
                 view.$el.find('.datagrid-header-container').append(view.filter.render().el);
                 view.filter.$el.find('a.close').replaceWith('<i class="close icon-x"></i>');
+
+                //b. add filter icon to headers
+                view.$el.find('th').each(function(index, el){
+                    var $el = $(el);
+                    if($el.find('a').text().length>0){
+                        $el.append('<input class="columFilter" style="float:left;display:none;position:absolute;"><i class="icon-filter pull-right"></i>');
+                    }
+                });
                 
+            },
+
+            filtColumn: function(e){
+                var $el = $(e.currentTarget);
+                var $searchBox = $el.prev().toggle();
+                if($searchBox.offset()){
+                    var offset = $el.parent().offset();
+                    $searchBox.focus().offset({top: offset.top + ($el.parent().innerHeight()-$el.parent().height())/4 , left: offset.left + ($el.parent().innerWidth()-$el.parent().width())/4}).width($el.parent().width());
+                }
+
+            },
+
+            closeFilt: function(e){
+                var $el = $(e.currentTarget);
+                $el.hide();
             }
         },
     });
