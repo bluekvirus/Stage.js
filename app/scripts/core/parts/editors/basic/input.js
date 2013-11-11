3a66e3c5f4609ee5b1378731bcdea3c1d6456eb0
Tim Liu <bluekvirus@gmail.com>
2013-11-11 23:36:58 +0800

    Cont. (+input with placeholder/help/tooltip and radios/checkboxes)
    Need to test out the basic input editors before moving on to (getVal,
    setVal and validate + msg)

---
+++ app/scripts/core/parts/editors/basic/input.js
@@ -0,0 +1,138 @@
+/**
+ * This is the code template for **basic** <input> editor.
+ *
+ * Note that the validate function defaults on no-op. You should override this according to field settings during form/formPart init.
+ *
+ * Init Options
+ * ============
+ * name
+ * type (see core/parts/editors/README.md)
+ * label
+ * help
+ * tooltip
+ * options: { (for radios and checkboxes only - note that the choice data should be prepared and passed in instead of using url or callbacks to fetch within the editor)
+ * 	inline: true|false
+ * 	data: []
+ * 	labelField
+ * 	valueField
+ * }
+ * validate (custom function or settings to use Backbone.Validations)
+ *
+ * The validation function should return null or 'error string' to be used in status.
+ *
+ * @author Tim.Liu
+ * @created 2013.11.10
+ * @version 1.0.1
+ */
+
+Application.Editor.register('Input', function(){
+
+	var UI = Backbone.Marionette.ItemView.extend({
+
+		template: 'editor-Input-tpl',
+		className: 'control-group',
+
+		events: {
+			'change input': 'onChange',
+			'blur input': 'onBlur',
+			'focus input': 'onFocus'
+		},
+
+		initialize: function(options){
+			this.autoDetectUIs();
+			//collect [parentCt](to fire events on), name, label, type, placeholder/help/tooltip, options(radios/checkboxes only) and validation settings
+			this.parentCt = options.parentCt;
+			
+			if(options.options){
+				//radios, checkboxes
+				var choices = options.options;
+				if(_.isObject(choices.data[0])){
+					choices.valueField = choices.valueField || 'value';
+					choices.labelField = choices.labelField || 'label';
+					choices.data = _.map(choices.data, function(c){
+						return {value: c[choices.valueField], label: c[choices.labelField]};
+					});
+				}else {
+					choices.data = _.map(choices.data, function(c){
+						return {value: c, label: _.string.titleize(c)};
+					});
+				}
+			}
+			this.model = new Backbone.Model({
+				uiId: _.uniqueId('basic-editor-'),
+				name: options.name, //*
+				type: options.type, //*
+				label: options.label || '', //optional
+				placeholder: options.placeholder || '', //optional
+				help: options.help || '', //optional
+				tooltip: _.isString(options.tooltip) || '', //optional
+				options: options.options, //optional {inline: true|false, data:[{label:'l', val:'v', ...}, {label:'ll', val:'vx', ...}] or ['v', 'v1', ...], labelField:..., valueField:...}
+			});
+
+			if(options.validate) {
+				this.validate = function(){
+					//TBI
+				}
+			}
+
+			if(_.isObject(options.tooltip)){
+				//activate tooltip with specific config object - see /libs/bower_components/bootstrap[x]/docs/javascript.html#tooltips
+			}
+		},
+
+		setVal: function(){
+			throw new Error('DEV::Editor.Input::Has not yet implemented setVal()!');
+		},
+
+		getVal: function(){
+			throw new Error('DEV::Editor.Input::Has not yet implemented getVal()!');
+		},
+
+		validate: $.noop,
+
+		status: function(status, msg){
+			//set or get status of this editor UI
+			if(status){
+				//set warning, error, info, success... status, no checking atm.				
+				this.$el
+					.removeClass(this.$el.data('status'))
+					.addClass(status)
+					.data('status', status);
+				this.ui.msg.html(msg || '');
+			}else {
+				//get
+				return this.$el.data('status');
+			}
+		},
+
+		//need to forward events if has this.parentCt
+		onChange: $.noop,
+		onBlur: $.noop,
+		onFocus: $.noop
+
+	});
+
+	return UI;
+
+});
+
+Template.extend('editor-Input-tpl', [
+	'<label class="control-label" for="{{uiId}}">{{label}}</label>',
+	'<div class="controls">',
+		//checkboxes/radios
+		'{{#if options}}',
+			'<div id={{uiId}} data-toggle="tooltip" title="{{tooltip}}">',
+			'{{#each options.data}}',
+				'<label class="{{../type}} {{#if ../options.inline}}inline{{/if}}">',
+					'<input type="{{../type}}" value={{value}}> {{label}}',
+				'</label>',
+			'{{/each}}',
+			'</div>',
+		//normal single field
+		'{{else}}',
+			'<input type="{{type}}" id="{{uiId}}" placeholder="{{placeholder}}" data-toggle="tooltip" title="{{tooltip}}">'
+		'{{/if}}',
+		'<span class="help-block" ui="msg">{{msg}}</span>',
+		'<span class="help-block">{{help}}</span>'
+	'<div>'
+]);
