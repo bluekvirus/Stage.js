e4b8b895de3cd1f3ed69250895925b1dcce4421b
Tim Liu <bluekvirus@gmail.com>
2015-07-30 21:08:24 -0700

    Added stop.sh to pair with start.sh;

--- tools/start.sh
+++ tools/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-forever start -c nodemon ./devserver/run.js --exitcrash
+forever start --uid stagejsv1dev -a -c nodemon ./devserver/run.js --exitcrash
 
 echo 'devserver started'
---
+++ tools/stop.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+forever stop stagejsv1dev
+
+echo 'devserver stopped'
