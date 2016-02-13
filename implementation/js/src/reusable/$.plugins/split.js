49ebcc5c370510393ad16eaa277451f972d17ade
patrick zhu <patrickzhu510@gmail.com>
2016-02-12 16:01:21 -0800

    continued modify split.js

--- implementation/js/src/reusable/$.plugins/split.js
+++ implementation/js/src/reusable/$.plugins/split.js
@@ -27,6 +27,11 @@
 			$this = $(this);
 		//check the type of layout, bootstrap or free
 		if( type === 'bootstrap' /*bootstrap layout*/){
+			//hornor the height and width settings
+			if( height !== 'auto' )
+				$this.css({height: height});
+			if( width !== 'auto' )
+				$this.css({width: width});
 			//break split into tokens in the trimmed array
 			var bsgrid = /(xs|sm|md|lg)/,
 				trimmed = [];
@@ -55,10 +60,10 @@
 					//--------------------------------------------------------------------------------------//
 					//check whether given a region or view name
 					if(data[1]){
-						if( data[1].charAt(0) === data[1].charAt(0).toUpperCase() )
+						if( data[1].charAt(0) === data[1].charAt(0).toUpperCase() )//view name uppercase
 							rvname = 'view="' + data[1]+'"';
-						else if( data[1].charAt(0) === data[1].charAt(0).toLowerCase() )
-							rvname = 'region"=' + data[1]+'"';
+						else if( data[1].charAt(0) === data[1].charAt(0).toLowerCase() )//region name lowercase
+							rvname = 'region="' + data[1]+'"';
 						else
 							throw new Error('Dev::runtime::split-plugin::the region/view name you give is not valid.');
 					}
@@ -89,12 +94,97 @@
 				throw new Error('Dev::runtime::split-plugin::direction can only be \'h\' or \'v\' for horizontal or vertical respectively.');
 			}
 		}else if( type === 'free' /*free layout*/){
-			
+			//hornor the height and width settings
+			if( height !== 'auto' )
+				$this.css({height: height});
+			if( width !== 'auto' )
+				$this.css({width: width});
+			//make sure the parent position is at least relative
+			if($this.css('position') !== 'absolute' && $this.css('position') !== 'relative')
+				$this.css({position: 'relative'});
+			//check direction
+			if( direction === 'h' || direction === 'v' ){
+				//call setLayout function
+				setFreeLayout($this, split, direction, adjustable, barclass);
+			}else{
+				throw new Error('Dev::runtime::split-plugin::direction can only be \'h\' or \'v\' for horizontal or vertical respectively.');
+			}
 		}else{
 			//error layout type
 			throw new error('type can only be bootstrap or free; flexbox has not been implemented');
 		}
 
+	};
+
+	var px_em = function(array){
+		var bool = false,
+			match = /(px|em)/;
+		for(var i = 0; i < array.length; i++){
+			if( array[i].split(':')[0].match(match) ){
+				bool = true;
+				break;
+			}
+		}
+		return bool;
+	};
+
+	var setFreeLayout = function($el, array, direction, adjustable, barclass){
+		var sum = 0,
+			trimmed = [],
+			barPercent,
+			totalHeight = $el.height(),
+			contentHeight,
+			current = 0;
+		//get divide bar width by adding a barclass div, getting width and removing it
+		$el.append('<div class="'+barclass+'"></div>');
+		var barwidth = $el.find('.'+barclass)[(direction === 'h')? 'height' : 'width']();
+		barPercent = ( barwidth / totalHeight ) * 100;
+		$el.find('.'+barclass).remove();
+		//check the given height, whether there is a fixed number in it.
+		if( px_em(array) ){
+			//take out the fixed pixel or ems, then calcualte the percentage for every block
+			//insert divs and divide bars
+			
+			//if adjustable register resize event on the divide bar
+			
+			
+		}else{//calculate the percentage for every block, then insert divs and divde bars
+			//calculate the total ratio for all blocks
+			_.each(array, function(data, index){
+				//store array into 
+				trimmed[index] = data.split(':');
+				//add total propotion
+				sum += Number.parseInt(trimmed[index][0]);
+			});
+			contentHeight = totalHeight - ( array.length - 1 ) * barwidth;
+			_.each(trimmed, function(data, index){
+				var rvname = '',
+					position = (data[2])?'fixed':'absolute',
+					contentPercent = ( ( contentHeight * ( data[0] / sum ) ) / totalHeight ) * 100;
+				//check whether given a region or view name
+				if(data[1]){
+					if( data[1].charAt(0) === data[1].charAt(0).toUpperCase() )
+						rvname = 'view="' + data[1]+'"';
+					else if( data[1].charAt(0) === data[1].charAt(0).toLowerCase() )
+						rvname = 'region="' + data[1]+'"';
+					else
+						throw new Error('Dev::runtime::split-plugin::the region/view name you give is not valid.');
+				}
+				if( direction === 'h' ){
+					//insert contents horizontally if 'h'
+					$el.append('<div ' + rvname + ' style="top:' + current + '%;height:' + contentPercent + '%;position:' + position + ';"></div>');
+					current += contentPercent;
+					//add divide bar
+					if( index < ( array.length - 1 ) ){
+						$el.append('<div class="' + barclass + '"></div>');
+						current += barPercent;
+					}
+				}else{
+
+				}
+			});
+			
+		}
 	};
 
 })(jQuery);
--- implementation/site/view/demo/split.js
+++ implementation/site/view/demo/split.js
@@ -1,24 +1,23 @@
 ;(function(app){
 
 	app.regional('Demo.Split', {
-		className: 'row',
 		template: [
-			'<div class="col-md-6 wrapper-full" region="split-view1"></div>',
-			'<div class="col-md-6 wrapper-full" region="split-view2"></div>',
-			'<div class="col-md-6 col-md-offset-3 wrapper-full" region="split-view3"></div>',
-			'<div class="col-md-6 col-md-offset-3 wrapper-full" id="test" style="height:15em;width:25em;border:1px solid black;">I am test div</div>',
+			//'<div class="col-md-6 wrapper-full" region="split-view1"></div>',
+			//'<div class="col-md-6 wrapper-full" region="split-view2"></div>',
+			//'<div class="col-md-6 col-md-offset-3 wrapper-full" region="split-view3"></div>',
+			'<div style="height:15em;width:35em;border:1px solid black;"><div id="test"></div></div>',
 		],
 		onShow: function(){
-			this.$el.find('[region="split-view1"]').css({height: '25em'});
+			/*this.$el.find('[region="split-view1"]').css({height: '25em'});
 			this.$el.find('[region="split-view2"]').css({height: '25em'});
 			this.$el.find('[region="split-view3"]').css({height: '25em'});
 			this.getRegion('split-view1').show(new View1());
 			this.getRegion('split-view2').show(new View2());
-			this.getRegion('split-view3').show(new View3());
+			this.getRegion('split-view3').show(new View3());*/
 			//for test
 			this.$el.find('#test').split({
-				split: ['xs-6,md-4:sample-region', 'md-6:Sample-view', 'xs-6,sm-4'],
-				type: 'bootstrap',
+				split: ['1:sample-region', '2:Sample-view', '3::'],
+				direction: 'h',
 			});
 		}
 	});
