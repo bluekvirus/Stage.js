b83f46e20222010fdeb718e50501102a23acd4fd
Tim Lauv <bluekvirus@gmail.com>
2016-12-25 11:33:26 -0800

    Update start.sh

--- tools/start.sh
+++ tools/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-forever start --uid "stagejsv1dev" --watchDirectory ./devserver -w -a ./devserver/run.js
+forever start --uid "stagejsv1dev" --watch --watchDirectory ./devserver --append ./devserver/run.js
 
 echo 'Server started through forever, with --uid stagejsv1dev and auto change reload.'
