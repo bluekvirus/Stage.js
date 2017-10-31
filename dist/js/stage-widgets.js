415932dbc5a61da295c247bcb461895d84846793
Tim Lauv <bluekvirus@gmail.com>
2017-10-31 00:04:42 -0700

    Fixed double navigate-to events error upon multiple ready;

--- dist/js/stage-widgets.js
+++ dist/js/stage-widgets.js
@@ -196,6 +196,7 @@
 				}
 			}
 		});
+		
 		return UI;
 
 	});
@@ -240,6 +241,7 @@
 		});
 
 		return UI;
+		
 	});
 
 })(Application);
@@ -253,18 +255,20 @@
 ;(function(app){
 
 	app.widget('SeqCell', function(){
+
 		var UI = app.view({
 			template: '{{index}}'
 		});
 
 		return UI;
+		
 	});
 
 })(Application);
 ;/**
  * This is the ActionCell definition 
  *
- * options
+ * Options
  * -------
  * passed down by this.model.get('actions')
  * 
@@ -358,7 +362,7 @@
  * 	...
  * </ul>
  *
- * options
+ * Options
  * -------
  * 1. data - [{
  * 		val: ...
@@ -369,7 +373,7 @@
  *
  * 3. onSelected: callback
  *
- * override node view
+ * Override node view
  * ------------------
  * a. just template (e.g val attr used in template)
  * use node: {template: [...]}; don't forget <ul></ul> at the end of tpl string.
@@ -381,7 +385,7 @@
  * 		}
  * }
  *
- * note
+ * Note
  * ----
  * support search and expand a path (use $parent in node/leaf onSelected()'s first argument)
  *
@@ -471,18 +475,18 @@
 ;/**
  * Passive Paginator widget used with lists (CollectionView instances)
  *
- * options
+ * Options
  * -------
  * 0. target [opt] - target list view instance
  * 1. currentPage
  * 2. totalPages
  * 3. pageWindowSize - 3 means [1,2,3,...,] or [...,4,5,6,...] or [...,7,8,9] - default on 5
  *
- * format
- * ------
+ * UI format
+ * ---------
  * << [1,2,...] >>
  *
- * link with lists
+ * Link with lists
  * ---------------
  * trigger('view:change-page', page number)
  * 
@@ -497,6 +501,7 @@
 ;(function(app){
 
 	app.widget('Paginator', function(){
+
 		var UI = app.view({
 
 			className: 'pagination',
@@ -606,6 +611,101 @@
 		});
 
 		return UI;
+	});
+
+})(Application);
+;/**
+ * The Table of Content Widget
+ *
+ * Options
+ * -------
+ * 1. title - [optional] the title above the root <ul/>
+ * 2. template - [optional] the item template holding topic string
+ * 3. toc - the object or url that loads the toc metadata "Topic" --> "Markdown.md".
+ *
+ * 		{
+ * 			"1_topic A": "a.md",
+ * 		 	"2_topic B": {
+ * 			 	"a_overview": "b/-intro.md",
+ * 			  	"b_topic B.1": "b/b-1.md",
+ * 			   	"c_topic B.2": "b/b-2.md",
+ * 			    ...
+ * 		     },
+ * 		     ...
+ * 		},
+ * 		
+ * 4. onReady - [optional] what to do when the ToC view is ready
+ *
+ * Note
+ * ----
+ * Use "<number>_" or "<alphabetics>_" to indicate your topic order.
+ * You can use non empty view data:{} to reset toc. 
+ * There is an activation group called 'toc' for all the topic <li>, when clicked, emitting a global event 'load-topic-file' with file path.
+ * 
+ *
+ * @author Tim Lauv
+ * @created 2017.08.28
+ */
+
+(function(app){
+
+	app.widget('ToC', function(){
+
+		var UI = app.view({
+
+			defaults: {
+				//default configure
+				title: 'Table of Content',
+				template: '<a>{{.}}</a>',
+				toc: {
+					'1_Topic A': 'a.md',
+					'2_Topic B': {
+						'a_Overview': 'b/b-intro.md',
+						'b_Topic B-1': 'b/b-1.md',
+						'c_Topic B-2': 'b/b-2.md',
+						'd_Topic B-3': {
+							'1_Topic B-3-1': 'b/b-3/3.md',
+							'1_Topic B-3-2': 'b/b-3/2.md',
+						},
+					},
+					'3_Topic C': 'c.md',
+				}
+
+			},
+
+			onBeforeRender: function(){
+				//change the presentation after data loading but before render again
+				var tocCfgObj = _.extend({}, this.defaults, this.options); //can override title, template, toc with init options
+				tocCfgObj.toc = _.size(this.get())? this.get() : tocCfgObj.toc; //can override toc with data
+				this.template = '<h3>' + tocCfgObj.title + '</h3>' + helper(tocCfgObj.toc, _.partial(Marionette.Renderer.render, tocCfgObj.template));
+			},
+
+			onItemActivated: function($item, group){
+				switch(group){
+					case 'toc':
+						app.coop('load-topic-file', $item.attr('file'));
+					break;
+					default:
+					break;
+				}
+			}
+
+		});
+
+		function helper(tocPartialObj, itemTplFn){
+			var result = '<ul>';
+			_.each(tocPartialObj, function(mdFileStr, topicStr){
+				topicStr = topicStr.split('_')[1]; //removing the ordering bit before '_'
+				if(!_.isPlainObject(mdFileStr))
+					result += '<li activate="toc" file="' + mdFileStr + '">' + itemTplFn(topicStr) + '</li>';
+				else
+					result += '<li>' + itemTplFn(topicStr) + helper(mdFileStr, itemTplFn) + '</li>';
+			})
+			return result + '</ul>';
+		}
+
+		return UI;
+
 	});
 
 })(Application);
